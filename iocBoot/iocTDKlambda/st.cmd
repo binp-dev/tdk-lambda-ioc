cefabdf00bf96645b2715eed2ffbacc95e585a94
Dmitriy Lipovyy <lipovyy@inp.nsk.su>
2022-05-17 14:59:34 +0700

    Initial commit

---
+++ iocBoot/iocTDKlambda/st.cmd
@@ -0,0 +1,37 @@
+#!../../bin/linux-x86_64/TDKlambda
+
+#- You may have to change TDKlambda to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/TDKlambda.dbd"
+TDKlambda_registerRecordDeviceDriver pdbbase
+
+## Config stream protocol
+epicsEnvSet("STREAM_PROTOCOL_PATH", "${TOP}/protocols")
+
+#drvAsynIPPortConfigure ("PS1", "127.0.0.1:10000")
+
+drvAsynSerialPortConfigure ("PS1","/dev/ttyUSB0")
+asynSetOption ("PS1", 0, "baud", "9600")
+asynSetOption ("PS1", 0, "bits", "8")
+asynSetOption ("PS1", 0, "parity", "none")
+asynSetOption ("PS1", 0, "stop", "1")
+asynSetOption ("PS1", 0, "clocal", "N")
+asynSetOption ("PS1", 0, "crtscts", "N")
+asynSetOption ("PS1", 0, "ixon", "N")
+asynSetOption ("PS1", 0, "ixany", "N")
+
+## Load record instances
+dbLoadTemplate("db/fast_records.substitution")
+dbLoadTemplate("db/records.substitution")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=dima"
