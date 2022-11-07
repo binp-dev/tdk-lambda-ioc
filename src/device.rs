78dd5285925ae6b771cb541641e554dfe384e2b4
Alexey Gerasev <gerasyov@inp.nsk.su>
2022-11-07 11:35:30 +0000

    Create device struct, track modules on gitlab

--- src/device.rs
+++ src/device.rs
@@ -1,12 +1,13 @@
-use ferrite::{AnyVariable, Context, Downcast, ReadVariable, WriteVariable};
+use ferrite::*;
 
 pub struct Device {
-    volt_real: WriteVariable<i32>,
-    curr_real: WriteVariable<i32>,
-    over_volt_set_point: ReadVariable<i32>,
-    under_volt_set_point: ReadVariable<i32>,
-    volt_set: ReadVariable<i32>,
-    curr_set: ReadVariable<i32>,
+    ser_numb: WriteArrayVariable<u8>,
+    volt_real: WriteVariable<f64>,
+    curr_real: WriteVariable<f64>,
+    over_volt_set_point: ReadVariable<f64>,
+    under_volt_set_point: ReadVariable<f64>,
+    volt_set: ReadVariable<f64>,
+    curr_set: ReadVariable<f64>,
 }
 
 fn take_var<V>(ctx: &mut Context, name: &str) -> V
@@ -27,6 +28,7 @@ where
 impl Device {
     pub fn new(addr: u8, ctx: &mut Context) -> Self {
         Self {
+            ser_numb: take_var(ctx, &format!("PS{}:ser_numb", addr)),
             volt_real: take_var(ctx, &format!("PS{}:volt_real", addr)),
             curr_real: take_var(ctx, &format!("PS{}:curr_real", addr)),
             over_volt_set_point: take_var(ctx, &format!("PS{}:over_volt_set_point", addr)),
